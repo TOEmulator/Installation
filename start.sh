901d40d00650b3093b6d4d30bf52667b60d05f19
TOEmulator <zildjan01@gmail.com>
2025-11-23 11:48:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,45 +3,45 @@
 # Base directory (set this to your server root folder)
 BASE_DIR="$HOME/"
 
-# Start DB server in first tab
-konsole --new-tab -e bash -c "
-cd \"$BASE_DIR/db\" || exit
+# Start DB server in a new terminal
+lxterminal --title="DB Server" -e bash -c "
+cd \"$BASE_DIR/db\" || exit;
 if [ -f db_server.pid ]; then
-    rm -f db_server.pid
-    echo 'Removed old db_server.pid'
-fi
-sleep 2
-./db_server
-exec bash
+    rm -f db_server.pid;
+    echo 'Removed old db_server.pid';
+fi;
+sleep 2;
+./db_server;
+exec bash;
 " &
 
 # Wait before starting login server
 sleep 5
 
-# Start Login server in second tab
-konsole --new-tab -e bash -c "
-cd \"$BASE_DIR/login\" || exit
+# Start Login server in a new terminal
+lxterminal --title="Login Server" -e bash -c "
+cd \"$BASE_DIR/login\" || exit;
 if [ -f login_server.pid ]; then
-    rm -f login_server.pid
-    echo 'Removed old login_server.pid'
-fi
-sleep 2
-./login_server
-exec bash
+    rm -f login_server.pid;
+    echo 'Removed old login_server.pid';
+fi;
+sleep 2;
+./login_server;
+exec bash;
 " &
 
 
-# Wait before starting login server
+# Wait before starting game server
 sleep 10
 
-# Start Login server in second tab
-konsole --new-tab -e bash -c "
-cd \"$BASE_DIR/game\" || exit
+# Start Game server in a new terminal
+lxterminal --title="Game Server" -e bash -c "
+cd \"$BASE_DIR/game\" || exit;
 if [ -f game_server.pid ]; then
-    rm -f game_server.pid
-    echo 'Removed old game_server.pid'
-fi
-sleep 2
-./game_server
-exec bash
+    rm -f game_server.pid;
+    echo 'Removed old game_server.pid';
+fi;
+sleep 2;
+./game_server;
+exec bash;
 " &
